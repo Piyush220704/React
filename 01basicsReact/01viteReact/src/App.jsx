4aea0f80a97f14579ec374e2d0db626aedf3376e
Piyush Arora <piyusharora2207@gmail.com>
2024-11-07 00:48:43 +0530

    learn the basic structure of React and viteReact

---
+++ 01basicsReact/01viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai"
+
+function App() {
+
+
+  return (
+    // <h1>Hello World form React | Vite</h1>
+    <>
+      <Chai/>
+      <h1>chai is ready from h1 tag</h1>
+      <p>this is a test para</p>
+    </>
+  )
+}
+
+export default App
