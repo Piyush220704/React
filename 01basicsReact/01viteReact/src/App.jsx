dd4a3732790782ba137027519ebb23a6657803cd
Piyush Arora <piyusharora2207@gmail.com>
2024-11-07 07:39:50 +0530

    learn custom React component

--- 01basicsReact/01viteReact/src/App.jsx
+++ 01basicsReact/01viteReact/src/App.jsx
@@ -2,12 +2,13 @@ import Chai from "./Chai"
 
 function App() {
 
-
+  const username = "Piyush";
   return (
     // <h1>Hello World form React | Vite</h1>
+  
     <>
       <Chai/>
-      <h1>chai is ready from h1 tag</h1>
+      <h1>hell from {username}</h1>
       <p>this is a test para</p>
     </>
   )
